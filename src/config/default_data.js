255aafc35fcfb2161be3b95fb92432603acd1d06
Serge KOUAME <serge.kouame@groupemediasoft.com>
2025-09-30 17:59:05 +0000

    update #1

---
+++ src/config/default_data.js
@@ -0,0 +1,5 @@
+module.exports = {
+    default_status : ['En attente', 'Actif', 'Désactivé'],
+    operation_status : ['En cours', 'Succès', 'Rejeté'],
+    portefeuille_status : ['En attente', 'Validé', 'Rejeté'],
+}
--- src/config/default_statuts.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    default_status : ['En attente', 'Actif', 'Désactivé'],
-    operation_status : ['En attente', 'Validé', 'Réfusé', 'Supprimé']
-}
