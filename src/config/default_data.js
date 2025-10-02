1c93ac9cdaaa677991d52a842640ef3a7b89ac4d
Serge KOUAME <serge.kouame@groupemediasoft.com>
2025-10-02 12:23:11 +0000

    mise à jour du client #4

--- src/config/default_data.js
+++ src/config/default_data.js
@@ -2,6 +2,6 @@ module.exports = {
     default_status : {'0':'En attente', '1':'Actif', '2':'Bloqué', '3':'Désactivé', '-1':'Supprimé'},
     operation_status : {'0':'En cours', '1':'Succès', '2':'Rejeté', '-1':'Supprimé'},
     portefeuille_status : {'0':'En attente', '1':'Validé', '2':'Rejeté', '-1':'Supprimé'},
-    status_couleur : {'0':'grey', '1':'green', '2':'yellow', '3':'orange', '-1':'red'},
+    status_couleur : {'0':'grey', '1':'green', '2':'orange', '3':'grey', '-1':'red'},
     type_piece : {'0':'Autres', '1':'CNI', '2':'Passport', '3':'Permis', '4':'Carte_sejour'},
 }
